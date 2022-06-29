bcbbb94e4e8e18ec4424b86713b43c19d16eb877
oI._.do_young <73626287+LEE-DO-YOUNG-03-22@users.noreply.github.com>
2022-06-30 01:27:14 +0900

    Create project_1001.c

---
+++ project_1001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b ;
+    scanf("%d %d", &a, &b);
+    if( 0 < a || b < 10 ){
+        printf("%d\n", a - b);
+    }
+    else{
+        printf("error\n");
+    }
+}
