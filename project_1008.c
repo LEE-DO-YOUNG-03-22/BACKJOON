96f5cb92dcefd7a2b767cc957a24fbff4c38e121
oI._.do_young <73626287+LEE-DO-YOUNG-03-22@users.noreply.github.com>
2022-06-30 03:06:51 +0900

    Update project_1008.c

--- project_1008.c
+++ project_1008.c
@@ -3,10 +3,7 @@
 int main(){
     int a,b ;
     scanf("%d %d", &a, &b);
-    if( 0 < a || b < 10 ){
-        printf("%d\n", a / b);
-    }
-    else{
-        printf("error\n");
-    }
+    printf("%.9f\n", a/(double)b);
+    
+    return 0;
 }
