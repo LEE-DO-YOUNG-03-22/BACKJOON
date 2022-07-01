1d0bfcabca41cff3d5065730f81a6433e25d7aab
oI._.do_young <73626287+LEE-DO-YOUNG-03-22@users.noreply.github.com>
2022-07-02 01:19:09 +0900

    Create project_11021.c

---
+++ project_11021.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int A,B,T;
+    scanf("%d",&T);
+    for(int j = 0 ; j < T ; j++){
+        scanf("%d %d",&A,&B);
+        printf("Case #%d: %d\n", j + 1, A+B);
+    }
+    return 0;
+}
