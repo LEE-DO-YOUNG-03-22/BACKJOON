b73a90d35c9f348a76156b60b77a72e5802dba5b
oI._.do_young <73626287+LEE-DO-YOUNG-03-22@users.noreply.github.com>
2022-07-06 21:57:55 +0900

    Create project_1672.c

---
+++ project_1672.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void DNA_sequence();
+
+int main(){
+    int N;
+    char DNA[1000000];
+
+    scanf("%d",&N);
+    for(int i = 0 ; i < N ; i++){
+
+    }
+}
+
+void DNA_sequence(){
+
+}
